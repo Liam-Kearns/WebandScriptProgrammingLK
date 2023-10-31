aab722aa1c0ad6567abb4fef1a5a816103c96995
Liam-Kearns <144057134+Liam-Kearns@users.noreply.github.com>
2023-10-31 18:03:04 -0400

    AssignmentTwoCommit#6

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+
+
 router.get('/index', function(req, res, next) {
   res.render('index', { title: 'index' });
 });
